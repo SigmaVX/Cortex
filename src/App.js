88c6c06a2b6cd93f1a7950e5f6b46b5a09e41e43
SigmaVX <tonywible@gmail.com>
2018-06-14 14:58:31 -0400

    added random array function

--- src/App.js
+++ src/App.js
@@ -6,10 +6,24 @@ import Box from './components/Box';
 import cards from './cards.json';
 import './App.css';
 
+ 
+
+
+
+
 class App extends Component {
   
   // Saves Card Data To State
   state = { cards: cards }
+
+ 
+
+  shuffle = () => {
+    const order = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+    order.sort(function(a, b){return 0.5 - Math.random()});
+    console.log(order);
+    // return order;
+  }
   
   render() {
     return (
@@ -17,14 +31,17 @@ class App extends Component {
       <div>
         <Header/>
         <Body>
-          
-        {this.state.cards.map(card=> (
-          <Box
-          key={card.id}
-          id={card.id}
-          image={card.image}
-          // removeFriend={this.removeFriend}
-          />
+
+          {this.shuffle()}
+
+          {this.state.cards.map((card,i) => (
+            <Box
+            key={card.id}
+            id={card.id}
+            image={card.image}
+            order={card.order}
+            // removeFriend={this.removeFriend}
+            />
         
         ))}
 
--- src/components/Box.js
+++ src/components/Box.js
@@ -4,8 +4,8 @@ import React from "react";
 const Box = (props) => {
   
   return (
-  <div className="card col-2 mx-2 my-2 order-{props.order}" key={props.id} id={props.id} onClick={() => props.shuffle()}>
-      <img alt="Memory Image" src={props.image} />
+  <div className={`card col-2 mx-2 my-2 order-${props.order}`} key={props.id} id={props.id}>
+      <img alt="Memory Item" src={props.image} />
   </div>
   )
 };
