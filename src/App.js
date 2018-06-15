3a1e42971a5c8a9632a42a763e7d9fad8fd88167
SigmaVX <tonywible@gmail.com>
2018-06-15 14:43:33 -0400

    added new functions and state

--- src/App.js
+++ src/App.js
@@ -7,43 +7,56 @@ import cards from './cards.json';
 import './App.css';
 
  
-
-
-
-
 class App extends Component {
   
   // Saves Card Data To State
-  state = { cards: cards }
-
- 
-
-  shuffle = () => {
-    const order = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-    order.sort(function(a, b){return 0.5 - Math.random()});
-    console.log(order);
-    // return order;
+  constructor(props) {
+    super(props);
+    this.state = { 
+      cards: cards,
+      score: 0,
+      highScore: 0,
+      listDataFromChild: null
+    }
   }
-  
+
+  // Get Data From Child Component
+  getData = (dataFromChild) => {
+    this.setState({ listDataFromChild: dataFromChild });
+  }
+
+  // Random Shuffle Of Cards JSON Array 
+  shuffle = () => {
+    cards.sort(function(a, b){return 0.5 - Math.random()});
+    // this.setState({cards: cards});s
+  }
+
+  reload = () => {
+    window.location.reload();
+  }
+
+
   render() {
+    console.log(this.state.listDataFromChild);
     return (
 
       <div>
         <Header/>
         <Body>
 
-          {this.shuffle()}
+        {this.shuffle()}
 
-          {this.state.cards.map((card,i) => (
-            <Box
-            key={card.id}
-            id={card.id}
-            image={card.image}
-            order={card.order}
-            // removeFriend={this.removeFriend}
-            />
-        
+        {this.state.cards.map((card) => (
+          <Box
+          getData={this.getData}
+          key={card.id}
+          id={card.id}
+          image={card.image}
+          reload={this.reload}
+
+          />
         ))}
+
 
 
         </Body>
--- src/components/Box.js
+++ src/components/Box.js
@@ -1,13 +1,63 @@
 import React from "react";
 
 
-const Box = (props) => {
+
+class Box extends React.Component{
+
+  // Set Up Inital State
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: "false",
+      count: 0
+    };
+  }
+
+ 
   
-  return (
-  <div className={`card col-2 mx-2 my-2 order-${props.order}`} key={props.id} id={props.id}>
-      <img alt="Memory Item" src={props.image} />
-  </div>
-  )
-};
+  // Send Data To Parent (AKA App.JS)
+  sendData = () =>{
+    this.props.getData(this.state.count);
+  }
+
+
+  clickCheck = () => {
+     // Updating State & Send Data To Parent
+    if(this.state.clicked === "false"){
+      this.setState({clicked: "true"});
+      console.log("Total Clicked: ", this.state.count, " Status: ", this.state.clicked);
+      this.sendData();
+    } else {
+      // console.log("clicked");
+      
+      // this.sendData();
+    }
+  }
+
+  // how can i work with state
+  // how can i pass informtion from a sub component state to a parent state?
+  // how come i cannot write JS inside the constructors?
+  // do i need the constructor(props) to initiate state?
+  // exmplain the lifecycle hooks and how when to use them
+
+
+  render(){
+    // console.log(this.state);
+    return (
+        <div 
+          className="card col-2 mx-2 my-2" 
+          key={this.props.id} 
+          id={this.props.id}
+          clicked={this.state.clicked} 
+
+          onClick={() => this.clickCheck()}>
+          <img alt="Memory Item" src={this.props.image} />
+          {this.state.clicked}
+        </div>
+      )
+  };
+}
+
+// }
 
 export default Box;
